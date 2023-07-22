958b13f772543971feed2c8bc04da3e98d98753c
Keralkar <sheela.keralkar@hp.com>
2023-07-23 00:18:33 +0530

    Added the angular material components in the application.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,18 +2,29 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './components/login/login.component';
-import { ReactiveFormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { LandingPageComponent } from './components/landing_page/landing-page/landing-page.component';
+import {MatFormFieldModule} from '@angular/material/form-field'
+import {MatInputModule} from '@angular/material/input';
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    LandingPageComponent
   ],
   imports: [
     BrowserModule,
     ReactiveFormsModule,
-    BrowserAnimationsModule
+    FormsModule,
+    BrowserAnimationsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatIconModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,16 +1,29 @@
-import { Component } from '@angular/core';
-import {FormGroup, FormControl, FormBuilder, Validators} from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
 })
-export class LoginComponent {
-loginForm = this.formBuilder.group({
-  email: ['', Validators.required, Validators.email],
-  password: ['', Validators.required, Validators.minLength(8), Validators.maxLength(25), Validators.pattern('[a-zA-Z0-9]*'), Validators.pattern('[!@#$%^&*(),.?":{}|<>]')]
-});
+export class LoginComponent implements OnInit {
+  hide = false;
+  loginForm: FormGroup;
 
-constructor(private formBuilder: FormBuilder){}
+  constructor(private formBuilder: FormBuilder) {
+    this.loginForm = this.formBuilder.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: [
+        '',
+        [
+          Validators.required,
+          Validators.minLength(8),
+          Validators.maxLength(25),
+          Validators.pattern('(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]+')
+        ],
+      ],
+    });
+  }
+
+  ngOnInit() {}
 }
